22b1b7be00b40311a8df2009411a9af46fea10da
Seanán Ó Coistín <seananoc@gmail.com>
2020-03-11 14:41:13 +0000

    Updated HTML and JavaScript in the files

---
+++ api.js
@@ -0,0 +1,11 @@
+xhttp.open("POST", "https://cadhan.com/api/intergaelic/3.0", true);
+xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhttp.send("teacs=iongantach&foinse=ga");
+
+
+function test(param) {
+    console.log("shite");
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+}
+
+
