d0f19c4558251bc5629d3c58a1d9f0a51ffd5f25
Seanán Ó Coistín <seananoc@gmail.com>
2020-03-20 14:52:27 +0000

    This is adding the entries with their definitions and examples in a JavaScript file.

---
+++ words.js
@@ -0,0 +1,26 @@
+let wordArray = [
+{ 
+  word : "ábhacht",
+  gender : "f.1.",
+  definition : "caitheamh aimsire, aoibhneas",
+  examples: ["Comá ná tiocfadh olc ar an nduine bocht...", "Lorem ipsum dolor sit amet"]
+},
+{ 
+  word : "abair",
+  gender : "f.2.",
+  definition : "deirim, cuirimís i gcás, admhuighimís, bíodh agus go",
+  examples: ["This is a test.", "This is only a test."]
+},
+{ 
+  word : "acfuinn",
+  gender : "f.1.",
+  definition : "aithris, cur iarraidh ar, aithris...",
+  examples: ["Comá ná tiocfadh olc ar an nduine bocht...", "Lorem ipsum dolor sit amet"]
+},
+{ 
+  word : "test",
+  gender : "f.2.",
+  definition : "maoin, cómhacht, ionmhas, flúirse, neart, buaidh fuilngtheachta, feisteas",
+  examples: ["This is a test.", "This is only a test."]
+}
+];
