a6b2501f389165a7012b4440d2ef925a43cf4afe
Seanán Ó Coistín <seananoc@gmail.com>
2020-07-16 18:55:38 +0000

    A mish mash of various files and codes as I attempt to get this working

---
+++ assets/javascript/javascriptnew.js
@@ -0,0 +1,45 @@
+function queryString(form) {
+  return Array.from(form.querySelectorAll('textarea, [checked]'))
+    .map(el => {
+      const name = el.getAttribute('name');
+      return name + "=" + encodeURIComponent(form[name].value);
+    })
+    .join('&');
+}
+
+document.getElementById('IGform').addEventListener('submit', function(e) {
+  e.preventDefault();
+  document.getElementById('output').textContent = "Loading...";
+  fetch('https://cadhan.com/api/intergaelic/3.0', {
+      method: "post",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: queryString(this)
+    })
+    .then(r => r.json())
+    .then(reply => {
+      //document.getElementById('output').textContent = JSON.stringify(reply);
+      console.log(reply);
+      processPairs(reply);
+    });
+});
+
+function processPairs(arr) {
+  var div = document.getElementById("output");
+  var suppress = true;
+  div.innerHTML = '';
+  for (var i = 0; i < arr.length; i++) {
+    t = arr[i][1];
+    if (t == "\\n") {
+      div.innerHTML += '<br/>';
+      suppress = true;
+    } else {
+      if (suppress == false && !t.match(/^[.,\/;”:!?%})]$/)) {
+        div.innerHTML += " ";
+      }
+      suppress = (t.match(/^[“\/$(\[#{]$/) !== null);
+      div.innerHTML += t;
+    }
+  }
+}
