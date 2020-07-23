9577428a6b635031f8f6ea53882298c4115010a6
Seanán Ó Coistín <seananoc@gmail.com>
2020-07-23 12:38:49 +0000

    I deleted some files to tidy it up a bit

--- assets/javascript/words.js
+++ assets/javascript/words.js
@@ -1,42 +1,32 @@
 let wordArray = [
     {
         word: "ainbheart",
-        grammar: "b.3.",
+        grammar: "baininscneach - tríú díchlaonadh",
         definition: "droichghníomh, olc",
-        examples: ["Dá mhéid ar ghaibh sé tríd agus do deineadh mícheart air ní dheárna sé ainbheart riamh: beart nó gníomh olc.", "Cad eile é ach ainbheart luighe trom ar bhochtaibh? gníomh olc.", "Go maithidh Dia dho is iomdha ainbheart do rinne sé ag scriosadh na ndaoine as a mbailtibh: olca gránda."]
+        examples: ["Dá mhéid ar ghaibh sé tríd agus do deineadh mícheart air ní dheárna sé ainbheart riamh: beart nó gníomh olc.",
+        "Cad eile é ach ainbheart luighe trom ar bhochtaibh? gníomh olc.",
+        "Go maithidh Dia dho is iomdha ainbheart do rinne sé ag scriosadh na ndaoine as a mbailtibh: olca gránda."]
     },
     {
         word: "aithdheinim",
-        grammar: "b.a.",
+        grammar: "briathar a.",
         definition: "deinim airís, cuirim athchlódh air, aitheagar, iompuighim in athchuma, mar éadach, móin, féar",
         examples: ["Leag an ghaoth na stiocaidhe ach d’aithdheineas iad leis an ngealaigh: chuireas ’na seasamh fé sholas na gealaighe.", "An uile lá cuireann sé geall ar chapallaibh agus cailleann, ach ní thréigfeadh a mhian, ní aithdhéanfadh (aitéanfadh an fhoghraidheacht) an saoghal mór: ní atharróchadh faic a aigne.", "Bean bhaileach, ghasta a aithdheineas éadaighe dá chlainn seadh í: iompuigheann sí baill éadaigh lé duine aca agus deineann balcais mhaith dhe do dhuine is lugha dhíobh.", "Chaith an gabha an chrúdh do chur ’on teine bhí sí róchumhang, agus d’aithdhein sé oireamhnach í: dhein sé airís í i slighe go raibh sí mór a dóthain.", "Deineann sé agus aithdheineann sé an dán do léigheamh go dtí go mbíonn sé de ghlanmheabhair aige: léigheann go minic, minic é.", "Ná h-aithdhein do lámh do chur ar an iarann te nó is tinn duit: ná dein airís é.", "Bhuailis cheana me, aithdhein é, feicim: buail an athuair mé.", "Ní h-iontaoibh fear aithdhéanta: fear a iompuigheas ar fheabhas ó dhroichchleachtaibh.", "Tigh aithdhéanta: tigh atharruighthe ar fheabhas.", "Scaipeadh sinn don gcéad ionnsaidhe ón namhaid ach d’aithdheineamair agus ruagamair iad: thángamair i bhfóireamh a chéile airís.."]
     },
     {
         word: "aithne",
-        grammar: "b.4.",
+        grammar: "baininscneach 4ú díchlaonadh",
         definition: "coidreamh, fios nó eólas pearsanta",
         examples: ["Níl aon aithne agam ar an gcailín sin: níl a fhios agam cé h-í: ní fhéadfainn innsint duit cad is ainm di ná aon rud mar gheall uirthe.", "Chuireas aithne ar Thomás Ó Cléirigh n-a shiopa féin i Sráid an Pheárnéalaigh: choidrigheas é, cuireadh i bhfios dom cér bhé féin, a ainm agus mar sin de.", "Ní bhfaighinn ar m’aithne fear chómh galánta: ar líon na ndaoine go léir gur fios dom cé h-iad.", "Is maith an aithne, ní fuláir, atá agat in Áth Cliath tréis na mbliadhanta go léir atá caithte agat ann: a fhios agat cé h-iad mórán daoine, agus fios nó eólas cár cheart duit gabháil innte.", "Tréis na cinneamhna chaith sé an oidhche san ósbuidéal gan aithne: dall ar cad a bhí n-a aigne féin agus ar an uile rud n-a thímcheall.", "Trí lá thug sé gan aithne gan úrlabhra: gan bheith in inmhe aon rud do thabhairt fé ndeara ná focal cainnte do chur as.", "Tháinig sé isteach an doras mar bheadh an nimh ar an aithne agus sinn ag cainnt air: ar an uain go raibh sé in ár smaointibh.", "Mar bheadh an nimh ar an aithne rith sé im aigne dul fé n-a bhrághaid aguscad bheadh ná é marbh rómham ar an mbóthar: cómhacht éigin draoidheachta ag suathadh m’aigne.", "Aithne na féile agus na carthanachta a bhí agamsa air: sin mar chonnac é, fial, carthanach.", "Dob fhada fairsing aithne air: bhí a fhios ges na sluaightibh air i gcómhgar ’si gcéin.", "Dá mb’í aithne an bhitheamhnaigh do bheadh air ní fhéadfá gan cion do bheith agat air: teist, cáil bitheamhnaigh.", "Ná bíodh muinghin agat as dhuine gan aithne: duine nách mbeadh eólas agat cad é an saghas é.", "An bhfuil aithne agam air?” “tá, aithne thar na seacht n-aitheanntaibh”: aithne shármhaith agus le fada.", "Cuireadh ó aithne na h-áite me: coisceadh orm teacht ann.", "D’imthigh an oiread aimsire agus gur imthigheamair as aithne a chéile: gur dhearmhadamair aon rud ag baint linn a bhéarfadh chun ár gcuimhnte a chéile.", "D’ól sé an oiread san ná raibh aithne a bheart aige ag dul amugha a’s ag tabhairt dhá thaobh an bhóthair isteach: ní raibh aon mhotháilt aigne aige ar aon nidh a bhí dhá dhéanamh aige.", "Dá mbeadh aithne agat air leigfeá dho: dá mbeadh a fhios agat cad é an saghas (olc) atá ann.", "Cuir in aithne dá chéile me féin agus an cailín áluinn sin, led thoil: innis dom cé h-í agus innis di cé mé.", "Shaoileas go raibh seanaithne agam air, ach bhí dearmhad orm, ní chuirfinn ó m’aithne air, n-a leith go ndiúltóchadh sé aonne ar shuarachas: aithne an-mhaith.", "Dá mbeadh t’aithne agam thabharfainn mo rún duit: dá mbeadh oiread feasa agam id thaobh a’s go dtuigfí dhom go rabhais iontaobha.", "Duine cúthail nárbh fhuraiste aithne do chur air dob eadh Árt Ó Gríobhtha: a choidreamh do dhéanamh, a mheón do nochtadh.", "“Sagart gan sainnt, ní bhfaighinn ar m’aithne”: ameasc sagart mo choidrimh.", "Aithne gan choidreamh atá agat orm: is eól duit m’ainm ach ní h-eól duit mo mheón ná mo shlighte.", "Lé faid m’aimsire as an bparóiste téidheann sibh dem aithne: ní fheadar cé sibh féin."]
     },
     {
         word: "athnasc",
-        grammar: "f.1.",
+        grammar: "firinscneach, an chéad díchlaonadh",
         definition: "aithris, cur iarraidh ar, aithris cainnte duine d’fhonn bheith deimhnightheach di; aithris fhonmhóideach",
         examples: ["Ní dóigh liom gur chualais i gceart cad dubhairt sé, déin an chainnt d’athnasc air d’fhéachaint ar thugais leat i gceart í: aithris na focail do tuigeadh duit adubhairt sé.", "Cómhairle! ní théidheann lag orm ach ag athnasc air breis chéille do bheith aige: ag impidhe ’sag iarraidh go láidir.", "Go mbínn cortha bhínn ghá athnasc ar mo chlainn fanacht ó dhrochchuideachtain nó gur thinn dóibh: ghá athchuinghe ortha óm chroidhe.", "Comá ná tiocfadh olc ar an nduine bocht tutbhéalach nuair chonnaic sé ag athnasc air tu: ag aithris go fonmhóideach."]
     },
 ];
 
-let htmlString = "";
-for (var i = 0; i < wordArray.length; i++) {
-    console.log(': ' + wordArray[i]);
-    htmlString += "<h1>" + wordArray[i].word + "</h1>";
-    for (var e = 0; e < wordArray.grammar; e++);
-        htmlString += "<p>" + wordArray[i].grammar + "</p>"; {
-    }
-    for (var e = 0; e < wordArray.definition; e++);
-        htmlString += "<p>" + wordArray[i].definition + "</p>"; {
-    }
-    for (var e = 0; e < wordArray.examples; e++);
-        htmlString += "<p>" + wordArray[i].examples + "</p>"; {
-    }
-}
-document.getElementById("words").innerHTML = htmlString;
+htmlString += "<h2 onclick=\"test(this)\">"; 
+htmlString += wordArray[i].word;
+htmlString += "</h2>";
